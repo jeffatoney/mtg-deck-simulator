88ba2afc13fdad65f00a50f66ee3171ee7359f20
jeffatoney <jeff.a.toney@gmail.com>
2026-07-24 04:52:09 -0700

    Install copied-spell Commit correction

--- src/mtg_sim/__init__.py
+++ src/mtg_sim/__init__.py
@@ -1,11 +1,14 @@
 """Malcolm and Breeches simulator package."""
 
 from .phase5d_cards import install as _install_phase5d
+from .phase9f5_commit_fix import install as _install_phase9f5_commit
 from .phase9f5_fixes import install as _install_phase9f5
 
 __version__ = "0.1.0"
 
 _install_phase5d()
 _install_phase9f5()
+_install_phase9f5_commit()
 del _install_phase5d
 del _install_phase9f5
+del _install_phase9f5_commit
