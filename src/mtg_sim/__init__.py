5da964a225ea2ad1e305cb166706c51b3a545330
jeffatoney <jeff.a.toney@gmail.com>
2026-07-24 04:25:22 -0700

    Install Phase 9F-5 follow-up fixes

--- src/mtg_sim/__init__.py
+++ src/mtg_sim/__init__.py
@@ -1,8 +1,11 @@
 """Malcolm and Breeches simulator package."""
 
 from .phase5d_cards import install as _install_phase5d
+from .phase9f5_fixes import install as _install_phase9f5
 
 __version__ = "0.1.0"
 
 _install_phase5d()
+_install_phase9f5()
 del _install_phase5d
+del _install_phase9f5
