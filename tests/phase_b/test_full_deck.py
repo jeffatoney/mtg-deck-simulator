8915884613c188629d3f5aa9473cda2a003272f2
jeffatoney <jeff.a.toney@gmail.com>
2026-08-03 18:03:31 -0700

    Update anti-overclaim coverage for Chart a Course

--- tests/phase_b/test_full_deck.py
+++ tests/phase_b/test_full_deck.py
@@ -55,6 +55,7 @@ def test_complete_reviewed_composition_has_no_fallback_or_execution_overclaim() 
             "By Force",
             "Cascade Bluffs",
             "Change the Equation",
+            "Chart a Course",
             "Command Tower",
             "Commit // Memory",
             "Crab Umbra",
