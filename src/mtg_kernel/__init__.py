f03a33a98f2b237873ecddcb44c86a3c3ebe6d2c
jeffatoney <jeff.a.toney@gmail.com>
2026-08-07 20:11:57 -0700

    Install strategic trigger target bridge

--- src/mtg_kernel/__init__.py
+++ src/mtg_kernel/__init__.py
@@ -3,7 +3,9 @@
 from mtg_kernel.engine import GameExecutor
 from mtg_kernel.factory import add_card, new_game
 from mtg_kernel.phase_b_runtime import install_phase_b_runtime
+from mtg_kernel.phase_b_trigger_targets import install_trigger_target_choices
 
 install_phase_b_runtime(GameExecutor)
+install_trigger_target_choices(GameExecutor)
 
 __all__ = ["GameExecutor", "add_card", "new_game"]
