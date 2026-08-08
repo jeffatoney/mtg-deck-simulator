c6d4c539ac024341fc62d1d5fa2c9df764119df9
jeffatoney <jeff.a.toney@gmail.com>
2026-08-08 00:33:44 -0700

    Install interaction conformance guards

--- src/mtg_kernel/__init__.py
+++ src/mtg_kernel/__init__.py
@@ -2,10 +2,12 @@
 
 from mtg_kernel.engine import GameExecutor
 from mtg_kernel.factory import add_card, new_game
+from mtg_kernel.interaction_rules_conformance import install_interaction_rules_conformance
 from mtg_kernel.phase_b_runtime import install_phase_b_runtime
 from mtg_kernel.phase_b_trigger_targets import install_trigger_target_choices
 
 install_phase_b_runtime(GameExecutor)
 install_trigger_target_choices(GameExecutor)
+install_interaction_rules_conformance(GameExecutor)
 
 __all__ = ["GameExecutor", "add_card", "new_game"]
